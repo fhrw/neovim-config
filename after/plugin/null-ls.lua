f0d79beff548afa0291d2f35e29fb87eca60f9c1
Felix Watson <felix.dork@gmail.com>
2023-06-05 15:27:56 +1000

    updates

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -1,11 +1,14 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.completion.spell,
-
-        null_ls.builtins.formatting.gofmt,
-        null_ls.builtins.formatting.prettierd
-    },
+	sources = {
+		null_ls.builtins.completion.spell,
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.gofmt,
+		null_ls.builtins.formatting.elm_format,
+		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.formatting.rustfmt,
+		null_ls.builtins.formatting.purs_tidy,
+		null_ls.builtins.diagnostics.cppcheck,
+	},
 })
--- lua/felix/remap.lua
+++ lua/felix/remap.lua
@@ -2,17 +2,14 @@ vim.g.mapleader = " "
 
 --stock useful stuff
 vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
-vim.keymap.set("n", "fs", function()
+vim.keymap.set("n", "<leader>q", vim.cmd.q)
+vim.keymap.set("n", "<leader>so", vim.cmd.so)
+vim.keymap.set("n", "<leader>w", function()
 	vim.lsp.buf.format()
 	vim.cmd.w()
 end)
-vim.keymap.set("n", "<leader>q", vim.cmd.q)
-vim.keymap.set("n", "<leader>so", vim.cmd.so)
 
 --windows
-vim.keymap.set("n", "<leader>wo", function()
-	vim.cmd.wincmd("o")
-end)
 vim.keymap.set("n", "sv", function()
 	vim.cmd.vsp()
 	vim.cmd.wincmd("l")
